6e2a7f2e42dfcd221a6448fbeb9a75107e16f6da
gisgiselle <brengimolina@gmail.com>
2022-01-28 19:16:05 -0600

    Lab9

--- routes/routeindex.js
+++ routes/routeindex.js
@@ -1,18 +1,37 @@
 const { render } = require('ejs');
 const express = require('express');
 const router = express.Router();
-//const Task = require('../model/task');
+const Post = require('../model/post');
 
 
 router.get('/', async function(req,res){
-  res.render('index');
+  let post = await Post.find()
+  res.render('index', {post})
 });
 
-
-router.get('/newPost', async (req,res) =>{
-  res.render('newPost');
+router.get('/newPost',async (req,res) =>{
+ res.render('newPost')
+ 
 });
 
+router.post('/newPost',async (req,res) =>{
+  let post = new Post(req.body)
+  await post.save()
+  res.redirect('/')
+});
+
+router.get('/edit/:id', async (req,res) =>{
+  let id = req.params.id
+  let post =await Post.findById(id)
+  res.render('edit', {post})
+});
+
+router.post('/edit/:id', async (req,res) =>{
+//var {id} = req.params;
+await Post.updateOne({_id:req.params.id}, req.body);
+res.redirect("/");
+
+});
 
 
 module.exports = router;
